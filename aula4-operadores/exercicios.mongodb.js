dc092311ef42f9466bc9130f30d3e7b6d5493b8c
Vinicius Leonardo Silveira <vinicius.silveira1503@gmail.com>
2026-03-12 10:45:33 -0300

    aula 4 att

--- aula4-operadores/exercicios.mongodb.js
+++ aula4-operadores/exercicios.mongodb.js
@@ -1,10 +1,12 @@
 // 1.
+// produtos com preco maior ou igual a 2000
 db.produtos.find({
     "preco": { "$gte": 2000 }
 })
 
 
 // 2.
+// produtos da categoria moveis com avaliacao maior ou igual a 4.5
 db.produtos.find({
     "$and": [
         { "categoria": "Moveis" },
@@ -14,6 +16,7 @@ db.produtos.find({
 
 
 // 3.
+// produtos com preco menor que 2000 ou estoque maior que 20
 db.produtos.find({
     "$or": [
         { "preco": { "$lt": 2000 } },
@@ -23,12 +26,14 @@ db.produtos.find({
 
 
 // 4.
+// produtos com avaliacao
 db.produtos.find({
     "avaliacao": { "$exists": true }
 })
 
 
 // 5.
+// produtos que nao sejam eletronicos ou com preco maior que 3000
 db.produtos.find({
     "$or": [
         { "$nor": [{ "categoria": "eletronicos" }] },
